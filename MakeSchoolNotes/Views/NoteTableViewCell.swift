782497e51ba758dfca8f5546ddede374a7d2b835
Aarushi Shah <aaru@Aarushis-MacBook-Pro.local>
2015-06-25 15:50:52 -0700

    "All the way up to using realm"

--- MakeSchoolNotes/Views/NoteTableViewCell.swift
+++ MakeSchoolNotes/Views/NoteTableViewCell.swift
@@ -11,4 +11,22 @@ import UIKit
 
 class NoteTableViewCell: UITableViewCell {
     
+    static var dateFormatter: NSDateFormatter = {
+        var formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+        }()
+
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel:UILabel!
+    
+    var note: Note? {
+        didSet {
+            if let note = note, titleLabel = titleLabel, dateLabel = dateLabel {
+                self.titleLabel.text = note.title
+                self.dateLabel.text = NoteTableViewCell.dateFormatter.stringFromDate(note.modificationDate)
+            }
+        }
+    }
 }
